9c2a66c41c7cc78f0e3322b309a9d38dbca0773e
hamidwise1 <hrawebdev@gmail.com>
2024-08-15 12:17:05 +0330

    add toast tests and window match media method

---
+++ tests/ToasterDemo.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import ToastDemo from '../src/components/ToastDemo';
+import { Toaster } from 'react-hot-toast';
+import user from '@testing-library/user-event';
+
+describe('ToastDemo', () => {
+ it('should render a toast', async () => {
+  render(
+   <>
+    <ToastDemo />
+    <Toaster />
+   </>
+  );
+
+  const button = screen.getByRole('button');
+  await user.click(button);
+  const toast = await screen.findByText(/success/i);
+  expect(toast).toBeInTheDocument();
+ });
+});
